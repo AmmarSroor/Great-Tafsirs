7551b43b4a336f0327f84632c2da6104f21a8255
AmmarSroor <91069802+AmmarSroor@users.noreply.github.com>
2021-10-13 00:42:14 +0300

    Add files via upload

---
+++ lib/features/great_tafsirs_feature/domain/entities/ayah_data.dart
@@ -0,0 +1,17 @@
+class AyahData {
+  final int number;
+  final String text;
+  final String juz;
+  final int page;
+  final int hizbQuarter;
+  final bool sajda;
+
+  AyahData({
+    required this.number,
+    required this.text,
+    required this.juz,
+    required this.page,
+    required this.hizbQuarter,
+    required this.sajda,
+  });
+}
---
+++ lib/features/great_tafsirs_feature/domain/entities/surah_data.dart
@@ -0,0 +1,13 @@
+import 'ayah_data.dart';
+
+class SurahData {
+  final int number;
+  final String name;
+  final List<AyahData> ayahs;
+
+  SurahData({
+    required this.number,
+    required this.name,
+    required this.ayahs,
+  });
+}
